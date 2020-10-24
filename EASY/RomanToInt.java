050d324079949f1b4e289bb3c736982637a5c945
Noah <32444724+Xisha2100@users.noreply.github.com>
2020-10-24 12:18:42 +0800

    第13题

---
+++ EASY/RomanToInt.java
@@ -0,0 +1,39 @@
+package top.nzhz;
+
+public class RomanToInt {
+    public int romanToInt(String s) {
+        int ans = 0;
+        int preNum = getValue(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            int num = getValue(s.charAt(i));
+            if (preNum < num)
+                ans -= preNum;
+            else
+                ans += preNum;
+            preNum = num;
+        }
+        ans +=preNum;
+        return ans;
+    }
+
+    private int getValue(char ch) {
+        switch (ch) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+}
