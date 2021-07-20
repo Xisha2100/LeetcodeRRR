661771157364613597cda99e5aa696d0d6498ac1
Noah <32444724+Xisha2100@users.noreply.github.com>
2021-07-20 19:23:46 +0800

    第十题未完成

---
+++ HARD/10.IsMatch.java
@@ -0,0 +1,18 @@
+package top.nzhz;
+
+
+public class IsMatch {
+    public boolean isMatch(String s, String p) {
+        int slen = s.length(), plen = p.length();
+
+        int i=0;
+        if(plen==0&&slen!=0)
+            return flase;
+        if(slen==0){
+            
+        }
+        while(i<slen){
+
+        }
+    }
+}
