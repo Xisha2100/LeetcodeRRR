1cadcd874582122b6910de25250e22e0718591fc
Noah <32444724+Xisha2100@users.noreply.github.com>
2021-07-29 00:35:39 +0800

    第十题改进中

--- HARD/10.IsMatch.java
+++ HARD/10.IsMatch.java
@@ -4,19 +4,28 @@ public class IsMatch {
     public boolean isMatch(String s, String p) {
         int sLen = s.length(), pLen = p.length();
 
-        if(pLen==0&&sLen!=0)
+        if (pLen == 0 && sLen != 0)
             return false;
-        if(sLen==0)
+        if (sLen == 0) {
+        }
 
 
-        for(int j =pLen-1; j>=0; j--){
-            if(p.charAt(j)=='*') {
-                j--;
-                while (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')
+        for (int j = pLen - 1, i = sLen - 1; j >= 0; j--) {
+            if (i > 0) {
+                if (p.charAt(j) == '*') {
+                    j--;
+                    while (i >= 0 && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'))
+                        i--;
+                } else if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
                     i--;
+                } else {
+                    return false;
+                }
+            } else {
+                if ()
             }
-            if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')
-
         }
     }
+
+
 }
