4121b00af131d391a832955154a26412c6899cdc
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2021-12-14 20:07:56 +0800

    148

--- ToolClass/ListNode.java
+++ ToolClass/ListNode.java
@@ -1,11 +1,20 @@
 package top.nzhz.leetcode.editor.cn;
 
+
 public class ListNode {
     int val;
     ListNode next;
 
-    ListNode(int x) {
-        val = x;
-        next = null;
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
+
