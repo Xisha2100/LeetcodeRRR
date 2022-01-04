1ed209193afca155d0251e055c615aae477f86a4
Xisha2100 <32444724+Xisha2100@users.noreply.github.com>
2022-01-05 00:08:48 +0800

    337

---
+++ MEDIUM/337.HouseRobberIii.java
@@ -0,0 +1,79 @@
+  //在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“
+//房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。 
+//
+// 计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。 
+//
+// 示例 1: 
+//
+// 输入: [3,2,3,null,3,null,1]
+//
+//     3
+//    / \
+//   2   3
+//    \   \ 
+//     3   1
+//
+//输出: 7 
+//解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7. 
+//
+// 示例 2: 
+//
+// 输入: [3,4,5,1,3,null,1]
+//
+//     3
+//    / \
+//   4   5
+//  / \   \ 
+// 1   3   1
+//
+//输出: 9
+//解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+// 
+// Related Topics 树 深度优先搜索 动态规划 二叉树 👍 1090 👎 0
+
+  
+  package top.nzhz.leetcode.editor.cn;
+  
+  public class HouseRobberIii{
+      public static void main(String[] args) {
+           Solution solution = new HouseRobberIii().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int[] ans=dfs(root);
+        return Math.max(ans[0],ans[1]);
+    }
+
+    //return: ans=int[2]
+    //ans[0]:include node
+    //ans[1]:exclude node
+    public int[] dfs(TreeNode node){
+        if(node==null) return new int[]{0,0};
+        int[] leftNum = dfs(node.left);
+        int[] rightNum = dfs(node.right);
+
+        int include=node.val+leftNum[1]+rightNum[1];
+        int exclude=Math.max(leftNum[1],leftNum[0])+Math.max(rightNum[0],rightNum[1]);
+
+        return new int[]{include,exclude};
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
